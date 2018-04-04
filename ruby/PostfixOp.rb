c1d155759ecc96d457eea6d218ed30e430afafb8
Thomas Campistron <thomas.campistron@etudiant.univ-lille1.fr>
2018-04-05 00:18:22 +0200

    add increment

--- ruby/PostfixOp.rb
+++ ruby/PostfixOp.rb
@@ -36,8 +36,41 @@ class PostfixOp < Expression
 		end
 	end
 
+	def jit_compile(env, jit_string)
+		if self.expression.token.getTokenId() != :Identifier
+			raise "Operation " + self.token.type.to_s + " can't apply to type: "+self.expression.token.svalue
+		end
+
+		case (self.token.getTokenId())
+		when :Increment
+			var_name = self.expression.token.svalue.to_s
+
+			jit_string <<  "\x48\x8b\x05" #movq ???(%rip), %rax
+			write_diff_to(jit_string, var_name)
+
+			jit_string << "\x48\xc7\xc3\x01\x00\x00\x00" #movq $1, %rbx
+
+			jit_string << "\x48\x01\xc3" # addq %rax, %rbx
+
+			jit_string <<  "\x48\x8d\x05" # lea (???(%rip)), %rax
+			write_diff_to(jit_string, var_name)
+
+			jit_string << "\x48\x89\x18" #movq %rbx, (%rax)
+
+			jit_string << "\x53" #pushq %rbx
+
+		end
+	end
+
 	def to_s()
 		return "PostfixOp " + self.token.to_s()
 		+ " (" + self.expression.to_s() + ")";
 	end
 end
+
+
+
+
+
+
+
