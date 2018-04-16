61fe6ca609890c6f20cc10e443eea036703fbc11
Thomas Campistron <thomas.campistron@etudiant.univ-lille1.fr>
2018-04-16 18:40:34 +0200

    while works

--- ruby/While.rb
+++ ruby/While.rb
@@ -1,6 +1,6 @@
 class While
-#	private Expression condition
-#	private List<Statement> body
+	#	private Expression condition
+	#	private List<Statement> body
 	attr_accessor :condition, :body, :nodeId
 
 	def initialize(condition, body)
@@ -21,6 +21,39 @@ class While
 		return Value.new(:Undefined)
 	end
 
+	def funjit_compile(jit_string, env)
+		before_condition = jit_string.size
+
+		self.condition.funjit_compile(jit_string, env)
+
+		jit_string << "\x58" #popq %rax
+		jit_string << "\x48\x83\xf8\x00" #cmp $0, %rax 
+
+		jit_string << "\x0F\x84" # "\xe9" # je ???
+		pos_jne = jit_string.size
+
+		jit_string << "\xcc\xcc\xcc\xcc" # breakpoint
+
+		jit_string << "\x90" # nop before compiling body
+		jit_string << "\x90" # nop before compiling body
+
+		for st in self.body
+			st.funjit_compile(jit_string, env)
+		end
+
+		jit_string << "\xe9" # jmp before_condition
+		write_int_as_4bytes(before_condition - jit_string.size - 4, jit_string)
+
+		jit_string << "\x90" # nop after compiling body
+		jit_string << "\x90" # nop after compiling body
+
+		nb_bytes_wrote = jit_string.size - pos_jne
+
+		# -4 is the size of jne
+		write_int_as_4bytes(nb_bytes_wrote - 4, jit_string, pos_jne)
+	end
+
+
 	def jit_compile(jit_string)
 		before_condition = jit_string.size
 
@@ -31,7 +64,7 @@ class While
 
 		jit_string << "\x0F\x84" # "\xe9" # je ???
 		pos_jne = jit_string.size
-		
+
 		jit_string << "\xcc\xcc\xcc\xcc" # breakpoint
 
 		jit_string << "\x90" # nop before compiling body
@@ -51,9 +84,8 @@ class While
 
 		puts "jne " + nb_bytes_wrote.to_s + " bytes forward"
 
-		# -2 is the size of jne
+		# -4 is the size of jne
 		write_int_as_4bytes(nb_bytes_wrote - 4, jit_string, pos_jne)
-		
 	end
 
 	def to_s()
