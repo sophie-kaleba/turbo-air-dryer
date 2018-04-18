c162d20c7d4e443e62ad345d554eda841e74b38f
Thomas Campistron <thomas.campistron@etudiant.univ-lille1.fr>
2018-04-18 13:11:11 +0200

    start global var in function

--- ruby/Literal.rb
+++ ruby/Literal.rb
@@ -52,7 +52,15 @@ class Literal < Expression
 
 				jit_string << "\x50" #pushq %rax (take the value of the var)
 			elsif $var_table[self.token.svalue] != nil
-				jit_compile(jit_string)
+				jit_string <<  "\x48\x8b\x05" #movq ???(%rip), %rax
+				# On récupère la dernière adresse de memory manager
+				current_addr = c_get_var_addr
+
+				var_addr = $var_table[self.token.svalue]
+				diff_rip = var_addr - (current_addr + jit_string.size) - 4 - 2
+				write_int_as_4bytes(diff_rip, jit_string)
+
+				jit_string << "\x50" #pushq %rax (take the value of the var)
 			else
 				raise "Variable \"#{self.token.svalue}\" not declared on line #{self.token.line}"
 			end
