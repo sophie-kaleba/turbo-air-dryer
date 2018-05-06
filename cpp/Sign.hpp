3f6703bbe97d96b52c5d55beb02b5c5c8feda33f
irevoire <irevoire@hotmail.fr>
2018-05-06 03:47:12 +0200

    start sign

---
+++ cpp/Sign.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+extern const std:unordered_map<int, std::string> SIGN_HASH;
+
+#define shift_right_unsigned_assignment 5 
+#define shift_right_unsigned 42
+#define strict_equal 43
+#define strict_not_equal 44
+#define shift_left_assignment 53
+#define shift_right_assignment 54
+#define equal 19
+#define less_or_equal 21
+#define greater_or_equal 23
+#define not_equal 28
+#define increment 37
+#define decrement 38
+#define exponentiation 39
+#define shift_left 40
+#define shift_right 41
+#define logical_and 45
+#define logical_xor 46
+#define logical_or 47
+#define plus_assignment 48
+#define minus_assignment 49
+#define mul_assignment 50
+#define div_assignment 51
+#define mod_assignment 52
+#define and_assignment 56
+#define xor_assignment 57
+#define or_assignment 58
+#define not_assignment 59
+#define colon  1
+#define comma  2
+#define semicolon  3
+#define full_stop  4
+#define plus  5
+#define minus  6
+#define asterisk  7
+#define slash  9
+#define open_parenthesis 12
+#define close_parenthesis 13
+#define open_bracket 14
+#define close_bracket 15
+#define open_brace 16
+#define close_brace 17
+#define assignment 18
+#define less_than 20
+#define greater_than 22
+#define ampersand 24
+#define pipe 25
+#define caret 26
+#define exclamation_mark 27
+#define question_mark 29
+#define percent 34
+#define tilde: 35
+
